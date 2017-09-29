ac238537dfa7dcbba2c483ae965e1d307cda4e9a
dividebysara <s.jade@bellevuecollege.edu>
2017-09-29 15:41:39 -0700

    Add unit test project, using static System.Console's, ReadKey()

---
+++ FridaForte/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static System.Console;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(ReadKey(), null);
+        }
+    }
+}
