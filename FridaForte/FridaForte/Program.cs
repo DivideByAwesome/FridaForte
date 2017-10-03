a53ea1ff3dd88fc1876add90032ab8b37d33da56
Roscoe Bass <RoscoeB@tlgclass.com>
2017-10-03 13:46:58 -0700

    Adjusted the speed for the typer, added a word wrapper, fixed unit test

--- FridaForte/FridaForte/Program.cs
+++ FridaForte/FridaForte/Program.cs
@@ -24,31 +24,53 @@ namespace FridaForte
             Location pharmacy = new Location("Pharmacy", $"Today {player.FirstName} gets a letter from her cousin who lives in Fort Point, California.\n\n\nLetter:\n\nDearest Cousin {player.FirstName},\n\n\nIt is with great sadness that I inform you that I have fallen ill with dysentery. Our little town of Fort Point does not have a doctor and I am running out of time. I would normally not ask this of you but I am in great distress. As you are the only pharmacist I know, I see no better person to help me in my time of need. Would you please come as soon as possible?\n\n\nYour loving cousin,\n\nAsher", "no hint for this scene", choices);
 
             Typer(pharmacy.Name);
-            string[] text = pharmacy.Message.Split(' ');
-            List<string> lines = text.Skip(1).Aggregate(text.Take(1).ToList(), (l, w) =>
-            {
-                if (l.Last().Length + w.Length >= 80)
-                    l.Add(w);
-                else
-                    l[l.Count - 1] += " " + w;
-                return l;
-            });
+            Typer(WordWrapper(pharmacy.Message));
 
 
 
-            Typer(pharmacy.Message);
-
-            
-            
             ReadKey(); // This command pauses the console so user has time to read it and dev has time to see results.
+        }
+
+        static string WordWrapper(string paragraph)
+        {
+            if (string.IsNullOrWhiteSpace(paragraph))
+            {
+                return string.Empty;
+            }
+
+            var approxLineCount = paragraph.Length / Console.WindowWidth;
+            var lines = new StringBuilder(paragraph.Length + (approxLineCount * 4));
+
+            for (var i = 0; i < paragraph.Length;)
+            {
+                var grabLimit = Math.Min(Console.WindowWidth, paragraph.Length - i);
+                var line = paragraph.Substring(i, grabLimit);
+
+                var isLastChunk = grabLimit + i == paragraph.Length;
+
+                if (isLastChunk)
+                {
+                    i = i + grabLimit;
+                    lines.Append(line);
+                }
+                else
+                {
+                    var lastSpace = line.LastIndexOf(" ", StringComparison.Ordinal);
+                    lines.AppendLine(line.Substring(0, lastSpace));
+
+                    //Trailing spaces needn't be displayed as the first character on the new line
+                    i = i + lastSpace + 1;
+                }
+            }
+            return lines.ToString();
         }
 
         static void Typer(string str)
         {
-            for(int i = 0; i < str.Length; i++)
+            for (int i = 0; i < str.Length; i++)
             {
                 Write(str[i]);
-                System.Threading.Thread.Sleep(10);
+                System.Threading.Thread.Sleep(8);
             }
             WriteLine();
         }
