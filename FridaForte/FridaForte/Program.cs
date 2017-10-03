9e8ecb7dc5c509394db64534a2a07e425d8be179
Roscoe Bass <RoscoeB@tlgclass.com>
2017-10-03 12:27:59 -0700

    started working on word warp

--- FridaForte/FridaForte/Program.cs
+++ FridaForte/FridaForte/Program.cs
@@ -24,7 +24,20 @@ namespace FridaForte
             Location pharmacy = new Location("Pharmacy", $"Today {player.FirstName} gets a letter from her cousin who lives in Fort Point, California.\n\n\nLetter:\n\nDearest Cousin {player.FirstName},\n\n\nIt is with great sadness that I inform you that I have fallen ill with dysentery. Our little town of Fort Point does not have a doctor and I am running out of time. I would normally not ask this of you but I am in great distress. As you are the only pharmacist I know, I see no better person to help me in my time of need. Would you please come as soon as possible?\n\n\nYour loving cousin,\n\nAsher", "no hint for this scene", choices);
 
             Typer(pharmacy.Name);
+            string[] text = pharmacy.Message.Split(' ');
+            List<string> lines = text.Skip(1).Aggregate(text.Take(1).ToList(), (l, w) =>
+            {
+                if (l.Last().Length + w.Length >= 80)
+                    l.Add(w);
+                else
+                    l[l.Count - 1] += " " + w;
+                return l;
+            });
+
+
+
             Typer(pharmacy.Message);
+
             
             
             ReadKey(); // This command pauses the console so user has time to read it and dev has time to see results.
