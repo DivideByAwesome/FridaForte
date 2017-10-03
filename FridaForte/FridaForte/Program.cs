a64606b8472b12665fa8478d783d7a523b8f2036
Alumneh Asefa <alemneh.asefa@outlook.com>
2017-10-03 14:21:56 -0700

    Can dynamiclly load game content to program.

--- FridaForte/FridaForte/Program.cs
+++ FridaForte/FridaForte/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Console; // Now lazy devs don't have to type "Console" while coding!
+// Be sure to check Newtonsoft.json is downloaded.
+using Newtonsoft.Json;
+using System.IO;
 
 namespace FridaForte
 {
@@ -17,6 +20,20 @@ namespace FridaForte
             //Location meadow = new Location("Meadow");
             //Location jail = new Location("Jail");
             //Location cousinHouse = new Location("Cousin's House");
+            string jsonFile = @"C:\Users\Student\Source\Repos\FridaForte\FridaForte\FridaForte\GameContent.json";
+            Location[] locations = JsonConvert.DeserializeObject<Location[]>(File.ReadAllText(jsonFile));
+            for(int i = 0; i < locations.Length; i++)
+            {
+                WriteLine(locations[i].Name);
+                WriteLine(locations[i].Message);
+                WriteLine("\n\n***********");
+                WriteLine("Choices");
+                WriteLine("***********");
+                WriteLine(locations[i].Choices[0]);
+                WriteLine(locations[i].Choices[1]);
+            }
+
+
 
             ReadKey(); // This command pauses the console so user has time to read it and dev has time to see results.
         }
